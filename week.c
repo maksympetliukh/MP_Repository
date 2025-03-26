e18a1f6cdd66f11176e3269d8c7fad17cf3db36a
maksympetliukh <maksympetliukh@icloud.com>
2025-03-26 20:03:04 +0100

    Add files via upload

---
+++ week.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+
+typedef enum {MON, TUE, WED, THU, FRI, SAT} week;
+
+sem_t sem;
+
+void* week_thread(void* arg) {
+    while(1) {
+        char input;
+        sem_wait(&sem);
+        input = getchar();
+        switch (input) {
+            case '1':
+                printf("Your day of is Monday\n");
+            break;
+            case '2':
+                printf("Your day of is Tuesday\n");
+            break;
+            case '3':
+                printf("Your day of is Wednesday\n");
+            break;
+            case '4':
+                printf("Your day of is Thursday\n");
+            break;
+            case '5':
+                printf("Your day of is Friday\n");
+            break;
+            case '6':
+                printf("Your day of is Saturday\n");
+            break;
+            case '7':
+                printf("Your day of is Sunday\n");
+            break;
+            case '8':
+                printf("Finish selection\n");
+            exit(0);
+            break;
+            default:
+                break;
+        }
+        sem_post(&sem);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t day_off;
+    sem_init(&sem, 0, 1);
+    pthread_create(&day_off, NULL, week_thread, NULL);
+
+    printf("Select your day off:\n");
+
+    pthread_join(day_off, NULL);
+    sem_destroy(&sem);
+
+    return 0;
+}
